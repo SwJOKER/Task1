9d381f156acf83d9a93c7b38408ac3b76b27318b
SwJOKER <63502467+SwJOKER@users.noreply.github.com>
2021-12-16 19:59:42 +0300

    ref

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-# This is a sample Python script.
+import logging
 import psutil
 import subprocess
 import time
 import xml.etree.ElementTree as ET
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
 INTERVAL = 2
 PROGRAM_PATH = "notepad.exe"
 
@@ -17,22 +16,25 @@ def get_info():
         print("Неверный путь")
         exit()
     process = psutil.Process(pid)
-    first = ET.Element("data")
-    first.set("interval", str(INTERVAL))
+    root = ET.Element("data")
+
+    root.set("interval", str(INTERVAL))
     i = 0
     while process.is_running():
-        se = ET.SubElement(first, str(i))
-        ET.SubElement(se, "handlers").text = str(process.num_handles())
-        ET.SubElement(se, "working_set").text = str(process.memory_info().wset)
-        ET.SubElement(se, "private_bytes").text = str(process.memory_info().private)
-        ET.SubElement(se, "cpu").text = str(process.cpu_percent())
-        print(f"Handlers: {process.num_handles()}")
-        print(f"Working Set: {process.memory_info().wset}")
-        print(f"Private Bytes: {process.memory_info().private}")
-        print(f"CPU: {process.cpu_percent()}%")
+        logging.basicConfig(level=logging.INFO)
+        stats = ET.SubElement(root, 'stats')
+        ET.SubElement(stats, 'time').text = str(time.time())
+        ET.SubElement(stats, 'handlers').text = str(process.num_handles())
+        ET.SubElement(stats, "working_set").text = str(process.memory_info().wset)
+        ET.SubElement(stats, "private_bytes").text = str(process.memory_info().private)
+        ET.SubElement(stats, "cpu").text = str(process.cpu_percent())
+        logging.info(f" Handlers: {process.num_handles()}" +
+                     f" Working Set: {process.memory_info().wset} " +
+                     f"Private Bytes: {process.memory_info().private}" +
+                     f" CPU: {process.cpu_percent()}%")
         time.sleep(int(INTERVAL))
         i += 1
-    ET.ElementTree(first).write("1.xml")
+    ET.ElementTree(root).write("1.xml")
 
 
 if __name__ == '__main__':
